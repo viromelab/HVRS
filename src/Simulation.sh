009b1a48a26fa0fe3c3f33105a828f1dd39576a9
pratas <6882743+pratas@users.noreply.github.com>
2022-09-16 11:26:28 +0100

    cleaning the code

--- src/Simulation.sh
+++ src/Simulation.sh
@@ -10,28 +10,28 @@ gto_fasta_extract_read_by_pattern -p "MG921180.1" < VDB.fa > HPV.fa
 #
 # MUTATE SEQUENCES:
 #
-gto_fasta_mutate -s 0 -e 0.01 < SVA.fa > SVA_1.fa
-gto_fasta_mutate -s 0 -e 0.03 < SVA.fa > SVA_2.fa
-gto_fasta_mutate -s 0 -e 0.05 < SVA.fa > SVA_3.fa
+gto_fasta_mutate -s 0 -e 0.01 < SVA.fa > SVA-1.fa
+gto_fasta_mutate -s 0 -e 0.03 < SVA.fa > SVA-2.fa
+gto_fasta_mutate -s 0 -e 0.05 < SVA.fa > SVA-3.fa
 #
-gto_fasta_mutate -s 0 -e 0.01 < B19.fa > B19_1.fa
-gto_fasta_mutate -s 0 -e 0.03 < B19.fa > B19_2.fa
-gto_fasta_mutate -s 0 -e 0.05 < B19.fa > B19_3.fa
+gto_fasta_mutate -s 0 -e 0.01 < B19.fa > B19-1.fa
+gto_fasta_mutate -s 0 -e 0.03 < B19.fa > B19-2.fa
+gto_fasta_mutate -s 0 -e 0.05 < B19.fa > B19-3.fa
 #
-gto_fasta_mutate -s 0 -e 0.01 < HPV.fa > HPV_1.fa
-gto_fasta_mutate -s 0 -e 0.03 < HPV.fa > HPV_2.fa
-gto_fasta_mutate -s 0 -e 0.05 < HPV.fa > HPV_3.fa
+gto_fasta_mutate -s 0 -e 0.01 < HPV.fa > HPV-1.fa
+gto_fasta_mutate -s 0 -e 0.03 < HPV.fa > HPV-2.fa
+gto_fasta_mutate -s 0 -e 0.05 < HPV.fa > HPV-3.fa
 #
-gto_fasta_mutate -s 0 -e 0.01 < VZV.fa > VZV_1.fa
-gto_fasta_mutate -s 0 -e 0.03 < VZV.fa > VZV_2.fa
-gto_fasta_mutate -s 0 -e 0.05 < VZV.fa > VZV_3.fa
+gto_fasta_mutate -s 0 -e 0.01 < VZV.fa > VZV-1.fa
+gto_fasta_mutate -s 0 -e 0.03 < VZV.fa > VZV-2.fa
+gto_fasta_mutate -s 0 -e 0.05 < VZV.fa > VZV-3.fa
 #
 #
 # CREATE DATASETS:
 #
-cat SVA_1.fa B19_1.fa HPV_1.fa VZV_1.fa > DS1.fa
-cat SVA_2.fa B19_2.fa HPV_2.fa VZV_2.fa > DS2.fa
-cat SVA_3.fa B19_3.fa HPV_3.fa VZV_3.fa > DS3.fa
+cat SVA-1.fa B19-1.fa HPV-1.fa VZV-1.fa > DS1.fa
+cat SVA-2.fa B19-2.fa HPV-2.fa VZV-2.fa > DS2.fa
+cat SVA-3.fa B19-3.fa HPV-3.fa VZV-3.fa > DS3.fa
 #
 #
 # SIMULATE FASTQ READS:
