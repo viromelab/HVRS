47bb1f46d2e488c65dbd2535a8d4ee2e6281572e
pratas <6882743+pratas@users.noreply.github.com>
2022-09-16 09:35:52 +0100

    installation

---
+++ Installation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# INSTALL SIMULATION AND EVALUATION TOOLS:
+#
+sudo apt-get install libopenblas-base
+conda install -c cobilab gto
+conda install -c bioconda art
+conda install -c bioconda mummer4
+#
+# INSTALL ASSEMBLY TOOLS:
+
+#
