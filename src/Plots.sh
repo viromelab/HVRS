fd421746dc2ebecab58b796b6e452911224c860b
mirakaya <59024914+mirakaya@users.noreply.github.com>
2023-04-17 17:55:14 +0100

    Corrections.

--- src/Plots.sh
+++ src/Plots.sh
@@ -1,41 +1,73 @@
 #!/bin/bash
 #
-cp reconstructed/total_stats.tsv .
-declare -a TOOLS=("coronaspades" "haploflow" "lazypipe" "metaspades" "metaviralspades" "pehaplo" "qure" "qvg" "spades" "ssake" "tracespipe" "tracespipelite" "virgena" "vispa")
+#cp reconstructed/total_stats.tsv .
+declare -a TOOLS=("coronaSPAdes" "Haploflow" "LAZYPIPE" "metaSPAdes" "metaviralSPAdes" "PEHaplo" "QuRe" "QVG" "SPAdes" "SSAKE" "TRACESPipe" "TRACESPipeLite" "VirGenA" "ViSpA")
 #declare -a list
 #declare -p list
 #
-for i in "${TOOLS[@]}" 
+rm -rf cnt*
+mkdir cnt3
+mkdir cnt0
+i=""
+declare ans
+check_condition() {
+  is_fst_set=$(printf '%s\n' "${FIRST_SET[@]}" | grep -w -- $dataset)
+  
+  if [ ! -z "$cov_2" ]
+    then
+    ans=1 
+  else
+    ans=0
+  fi
+}
+
+for tool in "${TOOLS[@]}" 
   do
-  $(cat total_stats.tsv | grep "${i}-" | awk '$14 == "1" { print $0 }' > ${i}.snp1)
-  echo "$(sort -t$'\t' -k13 -n ${i}.snp1)" > ${i}.snp1
-  $(cat total_stats.tsv | grep "${i}-" | awk '$14 == "0" { print $0 }' > ${i}.snp0)
-  echo "$(sort -t$'\t' -k13 -n ${i}.snp0)" > ${i}.snp0
+  
+  i=$(echo "$tool" | tr '[:upper:]' '[:lower:]' )
+  
+  cd cnt3  
+  $(cat ../total_stats.tsv | tr ',' '.' | grep "${i}-" | awk '$ 15 == "3" { print $0 } ' | awk '{if ($1=="DS9" || $1=="DS10" || $1=="DS11" || $1=="DS12" || $1=="DS13" || $1=="DS14" || $1=="DS15" || $1=="DS16") {print $0}}' > "${tool}")
+  echo "$(sort -t$'\t' -k13 -n ${tool})" > ${tool}
+  cd ..
+  
+  cd cnt0 
+  $(cat ../total_stats.tsv | tr ',' '.' | grep "${i}-" | awk '$15 == "0" { print $0 }' | awk '{if ($1=="DS1" || $1=="DS2" || $1=="DS3" || $1=="DS4" || $1=="DS5" || $1=="DS6" || $1=="DS7" || $1=="DS8") {print $0}}' > "${tool}")
+  echo "$(sort -t$'\t' -k13 -n ${tool})" > ${tool}
+  cd ..
 done
 #
-list_snp1=($(ls *.snp1))  
-list_snp0=($(ls *.snp0))
+list_cnt3=($(ls cnt3))  
+list_cnt0=($(ls cnt0))
 #  
 #printf "${list[*]} \n\n"
 #
+cd cnt0
+#
 gnuplot << EOF
     reset
     set terminal pdfcairo enhanced color font 'Verdade,9'
-    set output "Histogram_Identity_snp1.pdf"
+    set output "Identity_cnt0.pdf"
     set datafile separator "\t"
-    set yrange [97:100]
-    set xrange [0:45]
+    set yrange [94:100.5]
+    set xrange [0:42]
+    set key outside right bottom
     set xtics auto
     set ytics auto
-    set ylabel "Avg Identity"
+    set ylabel "Average Identity"
     set xlabel "Coverage"
-    set multiplot
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
     
     count = 1
-    do for [ file in "${list_snp1[@]}"]{   
-      
-      plot file u 13:5 with linespoints linestyle count
+    os = 94
+    do for [ file in "${list_cnt0[@]}"]{  
+      set key at 64, os
+      plot file u 13:5 title file with linespoints linestyle count
       count = count + 1
+      os = os + 0.47
+      unset offsets
     }
 EOF
 #
@@ -43,23 +75,152 @@ EOF
 gnuplot << EOF
     reset
     set terminal pdfcairo enhanced color font 'Verdade,9'
-    set output "Histogram_Identity_snp0.pdf"
+    set output "NCD_cnt0.pdf"
     set datafile separator "\t"
-    set yrange [97:100]
-    set xrange [0:45]
+    set yrange [0:1]
+    set xrange [0:42]
+    set key outside right bottom
     set xtics auto
     set ytics auto
-    set ylabel "Avg Identity"
+    set ylabel "NCD"
     set xlabel "Coverage"
-    set multiplot
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
     
     count = 1
-    do for [ file in "${list_snp0[@]}"]{   
-      
-      plot file u 13:5 with linespoints linestyle count
+    os = 0
+    do for [ file in "${list_cnt0[@]}"]{  
+      set key at 64, os
+      plot file u 13:6 title file with linespoints linestyle count
       count = count + 1
+      os = os + 0.072
+      unset offsets
     }
 EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NRC_cnt0.pdf"
+    set datafile separator "\t"
+    set yrange [0:0.11]
+    set xrange [0:42]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NRC"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cnt0[@]}"]{  
+      set key at 64, os
+      plot file u 13:7 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.008
+      unset offsets
+    }
+EOF
+#
+cp *.pdf ..
+cd ..
+#
+#
+#
+cd cnt3
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "Identity_cnt3.pdf"
+    set datafile separator "\t"
+    set yrange [94:100.5]
+    set xrange [0:42]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "Average Identity"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 94
+    do for [ file in "${list_cnt3[@]}"]{  
+      set key at 64, os
+      plot file u 13:5 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.47
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NCD_cnt3.pdf"
+    set datafile separator "\t"
+    set yrange [0:1]
+    set xrange [0:42]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NCD"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cnt3[@]}"]{  
+      set key at 64, os
+      plot file u 13:6 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.072
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NRC_cnt3.pdf"
+    set datafile separator "\t"
+    set yrange [0:0.13]
+    set xrange [0:42]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NRC"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cnt3[@]}"]{  
+      set key at 64, os
+      plot file u 13:7 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.0095
+      unset offsets
+    }
+EOF
+#
+cp *.pdf ..
+cd ..
+#
+#
 #set multiplot
 #plot file u 13:5 title file with linespoints linestyle count
 #rm -rf total_stats.tsv
