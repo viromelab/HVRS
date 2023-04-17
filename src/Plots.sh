ce6d824dc214d98045608c5eed62536b048860a6
mirakaya <59024914+mirakaya@users.noreply.github.com>
2023-04-17 19:10:10 +0100

    Corrections.

--- src/Plots.sh
+++ src/Plots.sh
@@ -6,8 +6,16 @@ declare -a TOOLS=("coronaSPAdes" "Haploflow" "LAZYPIPE" "metaSPAdes" "metaviralS
 #declare -p list
 #
 rm -rf cnt*
+rm -rf cvg*
+#rm -rf Graphs
 mkdir cnt3
 mkdir cnt0
+mkdir cvg2
+mkdir cvg5
+mkdir cvg10
+mkdir cvg20
+mkdir cvg40
+mkdir Graphs
 i=""
 declare ans
 check_condition() {
@@ -27,18 +35,50 @@ for tool in "${TOOLS[@]}"
   i=$(echo "$tool" | tr '[:upper:]' '[:lower:]' )
   
   cd cnt3  
-  $(cat ../total_stats.tsv | tr ',' '.' | grep "${i}-" | awk '$ 15 == "3" { print $0 } ' | awk '{if ($1=="DS9" || $1=="DS10" || $1=="DS11" || $1=="DS12" || $1=="DS13" || $1=="DS14" || $1=="DS15" || $1=="DS16") {print $0}}' > "${tool}")
+  $(cat ../total_stats.tsv | tr ',' '.' | grep "${i}-" | awk '{if ($1=="DS9" || $1=="DS10" || $1=="DS11" || $1=="DS12" || $1=="DS13" || $1=="DS14" || $1=="DS15" || $1=="DS16") {print $0}}' > "${tool}")
   echo "$(sort -t$'\t' -k13 -n ${tool})" > ${tool}
   cd ..
   
   cd cnt0 
-  $(cat ../total_stats.tsv | tr ',' '.' | grep "${i}-" | awk '$15 == "0" { print $0 }' | awk '{if ($1=="DS1" || $1=="DS2" || $1=="DS3" || $1=="DS4" || $1=="DS5" || $1=="DS6" || $1=="DS7" || $1=="DS8") {print $0}}' > "${tool}")
+  $(cat ../total_stats.tsv | tr ',' '.' | grep "${i}-" | awk '{if ($1=="DS1" || $1=="DS2" || $1=="DS3" || $1=="DS4" || $1=="DS5" || $1=="DS6" || $1=="DS7" || $1=="DS8") {print $0}}' > "${tool}")
   echo "$(sort -t$'\t' -k13 -n ${tool})" > ${tool}
   cd ..
+  
+  cd cvg2 
+  $(cat ../total_stats.tsv | tr ',' '.' | grep "${i}-" | awk '{if ($1=="DS17" || $1=="DS18" || $1=="DS19" || $1=="DS20" || $1=="DS21" || $1=="DS22" || $1=="DS23" || $1=="DS24") {print $0}}' > "${tool}")
+  echo "$(sort -t$'\t' -k13 -n ${tool})" > ${tool}
+  cd ..
+  
+  cd cvg5 
+  $(cat ../total_stats.tsv | tr ',' '.' | grep "${i}-" | awk '{if ($1=="DS25" || $1=="DS26" || $1=="DS27" || $1=="DS28" || $1=="DS29" || $1=="DS30" || $1=="DS31" || $1=="DS32") {print $0}}' > "${tool}")
+  echo "$(sort -t$'\t' -k13 -n ${tool})" > ${tool}
+  cd ..
+  
+  cd cvg10
+  $(cat ../total_stats.tsv | tr ',' '.' | grep "${i}-" | awk '{if ($1=="DS33" || $1=="DS34" || $1=="DS35" || $1=="DS36" || $1=="DS37" || $1=="DS38" || $1=="DS39" || $1=="DS40") {print $0}}' > "${tool}")
+  echo "$(sort -t$'\t' -k13 -n ${tool})" > ${tool}
+  cd ..
+  
+  cd cvg20 
+  $(cat ../total_stats.tsv | tr ',' '.' | grep "${i}-" | awk '{if ($1=="DS41" || $1=="DS42" || $1=="DS43" || $1=="DS44" || $1=="DS45" || $1=="DS46" || $1=="DS47" || $1=="DS48") {print $0}}' > "${tool}")
+  echo "$(sort -t$'\t' -k13 -n ${tool})" > ${tool}
+  cd ..
+  
+  cd cvg40 
+  $(cat ../total_stats.tsv | tr ',' '.' | grep "${i}-" | awk '{if ($1=="DS49" || $1=="DS50" || $1=="DS51" || $1=="DS52" || $1=="DS53" || $1=="DS54" || $1=="DS55" || $1=="DS56") {print $0}}' > "${tool}")
+  echo "$(sort -t$'\t' -k13 -n ${tool})" > ${tool}
+  cd ..
+  
+  
 done
 #
 list_cnt3=($(ls cnt3))  
 list_cnt0=($(ls cnt0))
+list_cvg2=($(ls cvg2))
+list_cvg5=($(ls cvg5))
+list_cvg10=($(ls cvg10))
+list_cvg20=($(ls cvg20))
+list_cvg40=($(ls cvg40))
 #  
 #printf "${list[*]} \n\n"
 #
@@ -127,7 +167,7 @@ gnuplot << EOF
     }
 EOF
 #
-cp *.pdf ..
+cp *.pdf ../Graphs
 cd ..
 #
 #
@@ -217,8 +257,460 @@ gnuplot << EOF
     }
 EOF
 #
-cp *.pdf ..
+cp *.pdf ../Graphs
 cd ..
+#
+#
+cd cvg2
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "Identity_cvg2.pdf"
+    set datafile separator "\t"
+    set yrange [85:100.5]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "Average Identity"
+    set xlabel "SNPs"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 85
+    do for [ file in "${list_cvg2[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:5 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 1.1
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NCD_cvg2.pdf"
+    set datafile separator "\t"
+    set yrange [0:1]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NCD"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cvg2[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:6 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.072
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NRC_cvg2.pdf"
+    set datafile separator "\t"
+    set yrange [0:0.13]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NRC"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cvg2[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:7 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.0095
+      unset offsets
+    }
+EOF
+#
+cp *.pdf ../Graphs
+cd ..
+#
+#
+#
+cd cvg5
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "Identity_cvg5.pdf"
+    set datafile separator "\t"
+    set yrange [85:100.5]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "Average Identity"
+    set xlabel "SNPs"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 85
+    do for [ file in "${list_cvg5[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:5 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 1.1
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NCD_cvg5.pdf"
+    set datafile separator "\t"
+    set yrange [0:1]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NCD"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cvg5[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:6 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.072
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NRC_cvg5.pdf"
+    set datafile separator "\t"
+    set yrange [0:0.13]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NRC"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cvg5[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:7 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.0095
+      unset offsets
+    }
+EOF
+#
+cp *.pdf ../Graphs
+cd ..
+#
+#
+#
+cd cvg10
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "Identity_cvg10.pdf"
+    set datafile separator "\t"
+    set yrange [85:100.5]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "Average Identity"
+    set xlabel "SNPs"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 85
+    do for [ file in "${list_cvg10[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:5 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 1.1
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NCD_cvg10.pdf"
+    set datafile separator "\t"
+    set yrange [0:1]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NCD"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cvg10[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:6 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.072
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NRC_cvg10.pdf"
+    set datafile separator "\t"
+    set yrange [0:0.13]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NRC"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cvg10[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:7 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.0095
+      unset offsets
+    }
+EOF
+#
+cp *.pdf ../Graphs
+cd ..
+#
+#
+#
+cd cvg20
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "Identity_cvg20.pdf"
+    set datafile separator "\t"
+    set yrange [85:100.5]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "Average Identity"
+    set xlabel "SNPs"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 85
+    do for [ file in "${list_cvg20[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:5 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 1.1
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NCD_cvg20.pdf"
+    set datafile separator "\t"
+    set yrange [0:1]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NCD"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cvg20[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:6 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.072
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NRC_cvg20.pdf"
+    set datafile separator "\t"
+    set yrange [0:0.13]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NRC"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cvg20[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:7 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.0095
+      unset offsets
+    }
+EOF
+#
+cp *.pdf ../Graphs
+cd ..
+#
+#
+#
+cd cvg40
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "Identity_cvg40.pdf"
+    set datafile separator "\t"
+    set yrange [85:100.5]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "Average Identity"
+    set xlabel "SNPs"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 85
+    do for [ file in "${list_cvg40[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:5 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 1.1
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NCD_cvg40.pdf"
+    set datafile separator "\t"
+    set yrange [0:1]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NCD"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cvg40[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:6 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.072
+      unset offsets
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NRC_cvg40.pdf"
+    set datafile separator "\t"
+    set yrange [0:0.13]
+    set xrange [0:0.16]
+    set key outside right bottom
+    set xtics auto
+    set ytics auto
+    set ylabel "NRC"
+    set xlabel "Coverage"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    os = 0
+    do for [ file in "${list_cvg40[@]}"]{  
+      set key at 0.24, os
+      plot file u 14:7 title file with linespoints linestyle count
+      count = count + 1
+      os = os + 0.0095
+      unset offsets
+    }
+EOF
+#
+cp *.pdf ../Graphs
+cd ..
+#
+#
+#
 #
 #
 #set multiplot
