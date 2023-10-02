703846eb52e792bdc921cbafe3ef79a8a79786a3
mirakaya <59024914+mirakaya@users.noreply.github.com>
2023-10-02 09:54:23 -0400

    Added new plots.

--- src/Plots2.sh
+++ src/Plots2.sh
@@ -31,7 +31,49 @@ for tool in "${TOOLS[@]}"
    
 done
 #
-list_tools=($(ls vir))  
+mkdir vir_2
+for tool in "${TOOLS[@]}" 
+  do
+  
+  i=$(echo "$tool" | tr '[:upper:]' '[:lower:]' )
+  
+  cd vir_2  
+  content=$(cat ../total_stats.tsv | tr ',' '.' | grep -w "${i}-*" | awk '{if ($1=="DS5" || $1=="DS13" || $1=="DS57" || $1=="DS58") {print $0}}' | cut --complement -d'S' -f1 > $tool)
+ 
+  #aux=$(cat "${tool}" | awk -F"," '{print $1}' | cut -d'S' -f2 | cut -d'	' -f1 )
+  #content= echo "$content
+#	$aux"
+  #echo $content > $tool
+  #tool+="\n"
+  #tool+=$aux
+  echo "$(sort -t$'\t' -k1 -n ${tool})" > ${tool}
+  cd ..
+   
+done
+#
+mkdir vir_3
+for tool in "${TOOLS[@]}" 
+  do
+  
+  i=$(echo "$tool" | tr '[:upper:]' '[:lower:]' )
+  
+  cd vir_3  
+  content=$(cat ../total_stats.tsv | tr ',' '.' | grep -w "${i}-*" | awk '{if ($1=="DS13" || $1=="DS61" || $1=="DS62") {print $0}}' | cut --complement -d'S' -f1 > $tool)
+ 
+  #aux=$(cat "${tool}" | awk -F"," '{print $1}' | cut -d'S' -f2 | cut -d'	' -f1 )
+  #content= echo "$content
+#	$aux"
+  #echo $content > $tool
+  #tool+="\n"
+  #tool+=$aux
+  echo "$(sort -t$'\t' -k1 -n ${tool})" > ${tool}
+  cd ..
+   
+done
+#
+list_tools=($(ls vir)) 
+list_tools_2=($(ls vir_2)) 
+list_tools_3=($(ls vir_3))  
 #  
 #printf "${list[*]} \n\n"
 #
@@ -137,5 +179,210 @@ gnuplot << EOF
 EOF
 #
 cp *.pdf ../Graphs
+cd ../vir_2
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "Identity_cont.pdf"
+    set datafile separator "\t"
+    
+    ymax = 100.5
+    ymin = 94
+    offset = ( ymax - ymin ) / 15.0    
+    set yrange [ymin:ymax]
+    set xrange [-0.2:3.2]
+    set key outside right top
+    set xtics ("5" 0, "13" 1, "57" 2, "58" 3)
+    set ytics auto
+    set ylabel "Average Identity"
+    set xlabel "Dataset"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    do for [ file in "${list_tools[@]}"]{  
+      set key at 4.75, ymax
+      plot file u 0:5:1 title file with linespoints linestyle count
+      count = count + 1
+      if(count == 9){
+        count = count + 1
+      }
+      ymax = ymax - offset
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NCSD_cont.pdf"
+    set datafile separator "\t"
+    
+    ymax = 1.05
+    ymin = 0
+    offset = ( ymax - ymin )/15.0   
+    set yrange [ymin:ymax]
+    set xrange [-0.2:3.2]
+    set key outside right top
+    set xtics ("5" 0, "13" 1, "57" 2, "58" 3)
+    set ytics auto
+    set ylabel "NCSD"
+    set xlabel "Dataset"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    do for [ file in "${list_tools[@]}"]{  
+      set key at 4.75, ymax
+      plot file u 0:6:1 title file with linespoints linestyle count
+      count = count + 1
+      if(count == 9){
+        count = count + 1
+      }
+      ymax = ymax - offset
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NRC_cont.pdf"
+    set datafile separator "\t"
+    
+    ymax = 1.05
+    ymin = 0
+    offset = ( ymax - ymin )/15.0   
+    set yrange [ymin:ymax]
+    set xrange [-0.2:3.2]
+    set key outside right top
+    set xtics ("5" 0, "13" 1, "57" 2, "58" 3)
+    set ytics auto
+    set ylabel "NRC"
+    set xlabel "Dataset"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    do for [ file in "${list_tools[@]}"]{  
+      set key at 4.75, ymax
+      plot file u 0:7:1 title file with linespoints linestyle count
+      count = count + 1
+      if(count == 9){
+        count = count + 1
+      }
+      ymax = ymax - offset
+      
+    }
+EOF
+cp *.pdf ../Graphs
+#
+#
+cd ../vir_3
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "Identity_length.pdf"
+    set datafile separator "\t"
+    
+    ymax = 100.5
+    ymin = 94
+    offset = ( ymax - ymin ) / 15.0    
+    set yrange [ymin:ymax]
+    set xrange [-0.2:2.2]
+    set key outside right top
+    set xtics ("13" 0, "61" 1, "62" 2)
+    set ytics auto
+    set ylabel "Average Identity"
+    set xlabel "Dataset"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    do for [ file in "${list_tools[@]}"]{  
+      set key at 3.3, ymax
+      plot file u 0:5:1 title file with linespoints linestyle count
+      count = count + 1
+      if(count == 9){
+        count = count + 1
+      }
+      ymax = ymax - offset
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NCSD_length.pdf"
+    set datafile separator "\t"
+    
+    ymax = 1.05
+    ymin = 0
+    offset = ( ymax - ymin )/15.0   
+    set yrange [ymin:ymax]
+    set xrange [-0.2:2.2]
+    set key outside right top
+    set xtics ("13" 0, "61" 1, "62" 2)
+    set ytics auto
+    set ylabel "NCSD"
+    set xlabel "Dataset"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    do for [ file in "${list_tools[@]}"]{  
+      set key at 3.3, ymax
+      plot file u 0:6:1 title file with linespoints linestyle count
+      count = count + 1
+      if(count == 9){
+        count = count + 1
+      }
+      ymax = ymax - offset
+    }
+EOF
+#
+#
+gnuplot << EOF
+    reset
+    set terminal pdfcairo enhanced color font 'Verdade,9'
+    set output "NRC_length.pdf"
+    set datafile separator "\t"
+    
+    ymax = 1.05
+    ymin = 0
+    offset = ( ymax - ymin )/15.0   
+    set yrange [ymin:ymax]
+    set xrange [-0.2:2.2]
+    set key outside right top
+    set xtics ("13" 0, "61" 1, "62" 2)
+    set ytics auto
+    set ylabel "NRC"
+    set xlabel "Dataset"
+    set multiplot layout 1,1
+    set rmargin 30
+    set key at screen 1, graph 1  
+    
+    count = 1
+    do for [ file in "${list_tools[@]}"]{  
+      set key at 3.3, ymax
+      plot file u 0:7:1 title file with linespoints linestyle count
+      count = count + 1
+      if(count == 9){
+        count = count + 1
+      }
+      ymax = ymax - offset
+      
+    }
+EOF
+cp *.pdf ../Graphs
+#
 cd ..
 #
